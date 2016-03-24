ab1b6ef153587b970ccc86deb8a8e59c861f19f2
Onlyhope <leeaaron326@gmail.com>
2016-03-24 13:58:11 -0400

    2nd Commit Node Class and Tree Class created

--- src/id3implementation/Id3Node.java
+++ src/id3implementation/Id3Node.java
@@ -5,10 +5,21 @@
  */
 package id3implementation;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Aaron
  */
 public class Id3Node {
+    
+    private ArrayList<Id3Node> children;
+    private String name;
+    
+    public Id3Node(String newName) {
+        children = new ArrayList<>();
+        name = newName;
+    }
+    
     
 }
