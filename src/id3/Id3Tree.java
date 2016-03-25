3557f79e07a094a3b3f2248ff66f2ffa0e98417d
Onlyhope <leeaaron326@gmail.com>
2016-03-25 05:23:50 -0400

    3rd Commit: Entropy and Root Node is implemented.

---
+++ src/id3/Id3Tree.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id3;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Id3Tree {
+    
+    private ArrayList<ArrayList<String>> data;
+    
+    private Id3Node root;
+    
+    /**
+     * Constructor
+     */
+    public Id3Tree() {
+        data = new ArrayList();
+    }
+    
+    // Class Methods
+    public void buildTree() {
+        File file = new File("");
+        String filePath = file.getAbsolutePath();
+        filePath = filePath + "\\test-file.txt"; // Real file: cse353-hw2-data.tsv
+        
+        Id3DataManager dm = new Id3DataManager();
+        dm.loadDataFile(filePath);
+        data = dm.getData();
+        
+        createRoot();
+    }
+    
+    private void createRoot() {
+        root = new Id3Node();
+        root.setNodeData(data);
+        boolean success = root.determineEntropy();
+        
+        System.out.println(root.getEntropy());
+    }
+    
+
+
+
+
+
+
+    
+    // Getters
+    
+    public Id3Node getRoot() {
+        return root;
+    }
+    
+    // Setters
+    
+    public void setData(ArrayList<ArrayList<String>> newData) {
+        data = newData;
+    }
+
+   
+
+    
+    
+}
