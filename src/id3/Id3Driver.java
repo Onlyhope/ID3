680bccfd5f71454a3a9a5fa1e3017fff139a886a
Onlyhope <leeaaron326@gmail.com>
2016-04-02 23:57:10 -0400

    7th Commit: Data Processing and Testing Works 60% accuracy achieved

--- src/id3/Id3Driver.java
+++ src/id3/Id3Driver.java
@@ -14,6 +14,9 @@ import java.util.Map;
  * @author Aaron
  */
 public class Id3Driver {
+    
+    private static Id3DataManager dm;
+    
     public static void main(String[] args) {
         
         
@@ -21,7 +24,7 @@ public class Id3Driver {
         String filePath = file.getAbsolutePath();
         filePath = filePath + "\\cse353-hw2-data.tsv"; // Real file: cse353-hw2-data.tsv, test file: test-file.txt
         
-        Id3DataManager dm = new Id3DataManager();
+        dm = new Id3DataManager();
         dm.loadDataFile(filePath);
         
         
@@ -30,12 +33,63 @@ public class Id3Driver {
         dm.processContinuousData(10, 100);        
         dm.processContinuousData(11, 100);       
         dm.processContinuousData(4, 1);
-        dm.removeDataSet(4);
         dm.removeDataSet(2);
+        dm.removeDataSet(4);
+        
+        double accuracy = validationTest(5, 4);
+        System.out.println(accuracy);
+        
+    }
+    
+    public static double compareResults(ArrayList<String> results, ArrayList<ArrayList<String>> data) {
+        double accuracy = 0;
+        
+        double posCount = 0;
+        double counter = 0;
+        int index = data.get(0).size() - 1;
+        
+        for (int i = 0; i < results.size() && i < data.size(); i++) {            
+            if (results.get(i).equals(data.get(i).get(index))) {
+                posCount++;
+            }
+            counter++;
+        }
+        
+        accuracy = (posCount / counter);
+        
+        return accuracy;
+    }
+    
+    /**
+     * Removes the part/split of the main array data.
+     * @param split
+     * @param part
+     * @return 
+     */
+    public static double validationTest(int split, int part) {
+        ArrayList<String> curArr = (ArrayList<String>) dm.getData().get(0);
+        int n = curArr.size();
+        
+        n /= split;
+        part--;
+        
+        dm.dataSplit(split);
+        
+        int beg = n * part;
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < dm.getData().size(); j++) {
+                dm.getData().get(j).remove(beg);
+            }
+        }
         
         Id3Tree id3Tree = new Id3Tree();
         id3Tree.buildTree(dm.getData());
-                
-        id3Tree.traverseTree();
+        ArrayList<ArrayList<String>> splitData = dm.getDataSplit1();
+        ArrayList<String> results = id3Tree.testData(splitData);
+        double accuracy = compareResults(results, splitData);
+        
+        return accuracy;
     }
+        
 }
